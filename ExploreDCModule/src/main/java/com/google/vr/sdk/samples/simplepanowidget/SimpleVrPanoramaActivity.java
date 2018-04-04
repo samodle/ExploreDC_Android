b025b05660a64c8d31cc275348b07422c8b01446
Dwight Shrute <sro27@cornell.edu>
2018-04-04 17:01:41 -0400

    see descriptions for tutorial, done all but dopwnload a file
    https://docs.aws.amazon.com/aws-mobile/latest/developerguide/how-to-integrate-an-existing-bucket.html

--- ExploreDCModule/src/main/java/com/google/vr/sdk/samples/simplepanowidget/SimpleVrPanoramaActivity.java
+++ ExploreDCModule/src/main/java/com/google/vr/sdk/samples/simplepanowidget/SimpleVrPanoramaActivity.java
@@ -31,6 +31,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.NumberPicker;
 
 import com.google.vr.sdk.widgets.common.VrWidgetView;
 import com.google.vr.sdk.widgets.pano.VrPanoramaEventListener;
@@ -44,18 +45,9 @@ import java.io.InputStream;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobileconnectors.s3.transferutility.*;
 import com.amazonaws.mobile.client.AWSMobileClient;
-import com.amazonaws.services.s3.AmazonS3Client;
-
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferType;
-import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
-
-import android.widget.NumberPicker;
-
-import org.w3c.dom.Text;
 
 /**
  * A basic PanoWidget Activity to load panorama images from disk. It will load a test image by
@@ -68,65 +60,6 @@ import org.w3c.dom.Text;
  * VrPanoramaView.Options.inputType.
  */
 public class SimpleVrPanoramaActivity extends Activity {
-/*
-    public void dowloadData() {
-        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
-            @Override
-            public void onComplete() {
-                downloadWithTransferUtility();
-            }
-        }).execute();
-    }
-
-*/
-    public void downloadWithTransferUtility() {
-
-        TransferUtility transferUtility =
-                TransferUtility.builder()
-                        .context(getApplicationContext())
-                        .awsConfiguration(AWSMobileClient.getInstance().getConfiguration())
-                        .s3Client(new AmazonS3Client(AWSMobileClient.getInstance().getCredentialsProvider()))
-                        .build();
-
-        TransferObserver downloadObserver =
-                transferUtility.download(
-                        "s3Folder/s3Key.txt",
-                        new File("/path/to/file/localFile.txt"));
-
-        // Attach a listener to the observer to get state update and progress notifications
-        downloadObserver.setTransferListener(new TransferListener() {
-
-            @Override
-            public void onStateChanged(int id, TransferState state) {
-                if (TransferState.COMPLETED == state) {
-                    // Handle a completed upload.
-                }
-            }
-
-            @Override
-            public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
-                float percentDonef = ((float)bytesCurrent/(float)bytesTotal) * 100;
-                int percentDone = (int)percentDonef;
-
-                Log.d("MainActivity", "   ID:" + id + "   bytesCurrent: " + bytesCurrent + "   bytesTotal: " + bytesTotal + " " + percentDone + "%");
-            }
-
-            @Override
-            public void onError(int id, Exception ex) {
-                // Handle errors
-            }
-
-        });
-
-        // If you prefer to poll for the data, instead of attaching a
-        // listener, check for the state and progress in the observer.
-        if (TransferState.COMPLETED == downloadObserver.getState()) {
-            // Handle a completed upload.
-        }
-
-        Log.d("YourActivity", "Bytes Transferrred: " + downloadObserver.getBytesTransferred());
-        Log.d("YourActivity", "Bytes Total: " + downloadObserver.getBytesTotal());
-    }
 
   private static final String TAG = SimpleVrPanoramaActivity.class.getSimpleName();
   /** Actual panorama widget. **/
@@ -142,7 +75,7 @@ public class SimpleVrPanoramaActivity extends Activity {
   private Uri fileUri;
   /** Configuration information for the panorama. **/
   private Options panoOptions = new Options();
-  private Options panoOptions2 = new Options();
+ // private Options panoOptions2 = new Options();
   private ImageLoaderTask backgroundImageLoaderTask;
 
   private TextView p1;
@@ -201,7 +134,7 @@ public class SimpleVrPanoramaActivity extends Activity {
     setContentView(R.layout.main_layout);
 
     //yo dawg i copied this code str8 from https://docs.aws.amazon.com/aws-mobile/latest/developerguide/how-to-integrate-an-existing-bucket.html
-      AWSMobileClient.getInstance().initialize(this).execute();
+    AWSMobileClient.getInstance().initialize(this).execute();
 
     // Make the source link clickable.
     TextView sourceText = (TextView) findViewById(R.id.source);
